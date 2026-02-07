443a4dc3cf5cedd54bf3ca202f810b7aaecdcfdb
creacress <alexis@webcresson.com>
2026-02-07 22:45:26 +0100

    Versel Analyse

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import SiteHeader from "@/app/components/SiteHeader";
 import SiteFooter from "@/app/components/SiteFooter";
 
+import { Analytics } from '@vercel/analytics/next';
+
 export const metadata = {
   title: "Crise Conscience",
   description:
@@ -41,6 +43,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <div className="cc-page-slot">{children}</div>
           </main>
           <SiteFooter />
+          <Analytics />
         </div>
       </body>
     </html>
